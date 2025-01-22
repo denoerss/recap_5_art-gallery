d35e2eab9cf73fe7ea671a7511dc5885876f7cc3
Herr <g.kerber@posteo.it>
2025-01-22 20:44:00 +0100

    add a styling component for one art piece

--- components/ArtPiecePreview.js
+++ components/ArtPiecePreview.js
@@ -1,25 +1,8 @@
-import Image from "next/image";
-import styled from "styled-components";
-
-const StyledImage = styled(Image)`
-  object-fit: cover;
-`;
-
-const StyledFigure = styled.figure`
-  position: relative;
-  width: 100%;
-  height: 100%;
-`;
-
-const StyledCaption = styled.figcaption`
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  padding: 1rem;
-  background-color: black;
-  width: 100%;
-  color: white;
-`;
+import {
+  StyledImage,
+  StyledFigure,
+  StyledCaption,
+} from "../components/ArtPiece.styled";
 
 export default function ArtPiecePreview({ artPiece }) {
   return (
