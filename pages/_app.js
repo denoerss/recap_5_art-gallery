b3b5af64f79687c65e8689126b647da224f44a52
Denise Albrecht <denise-albrecht@posteo.de>
2025-01-22 11:41:37 +0100

    Add fetch function to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,22 @@
 import GlobalStyle from "../styles";
+import { useSWR, SWRConfig } from "swr";
+
+const URL = "https://example-apis.vercel.app/api/art";
+const fetcher = (...args) => fetch(...args).then((response) => response.json());
 
 export default function App({ Component, pageProps }) {
+  const { data: artPieces, error, isLoading } = useSWR(URL, fetcher);
+  if (error) {
+    return <h2>Ups, Error!</h2>;
+  }
+  if (isLoading) {
+    return <h2>is loading...</h2>;
+  }
+
   return (
-    <>
+    <SWRConfig value={{ fetcher }}>
       <GlobalStyle />
-      <Component {...pageProps} />
-    </>
+      <Component {...pageProps} artPieces={artPieces} />
+    </SWRConfig>
   );
 }
